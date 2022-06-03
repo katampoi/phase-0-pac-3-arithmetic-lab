0bb486a8dc9ca35a0c8face16d07d0eacb14dbcc
katampoi <simonkatampoi@student.moringaschool.com>
2022-06-03 10:17:15 +0300

    Done.

---
+++ index.js
@@ -0,0 +1,30 @@
+function add(a, b) {
+    return a + b;
+}
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return a / b; 
+}
+function increment(n) {
+    return n += 1;
+}
+function decrement(n) {
+    return n -= 1;
+}
+function makeInt(n) {
+    console.log(parseInt(n, 10));
+    return parseInt(n, 10);
+}
+function preserveDecimal(n) {
+    console.log(parseFloat(n));
+    return parseFloat(n);
+}
+
+
